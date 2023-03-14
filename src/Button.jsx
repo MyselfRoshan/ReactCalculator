56048e1c65307aced2375c8bbb26c0e3c36aeb6c
MyselfRoshan <roshanghimire699@gmail.com>
2023-03-14 22:36:50 +0545

    feat: added button to display

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,4 +1,16 @@
-function Button({ butttonTxt }) {
-  return <button className="button">{butttonTxt}</button>;
+function Button({ butttonTxt, result, setResult }) {
+  function handleClick(e) {
+    setResult((prevResult) => {
+      const regexNumber = /d[0-9]|./;
+      if (regexNumber.test(e.target.innerText))
+        prevResult.concat(e.target.innerText);
+    });
+    console.log(result);
+  }
+  return (
+    <button className="button" onClick={(e) => handleClick(e)}>
+      {butttonTxt}
+    </button>
+  );
 }
 export default Button;
