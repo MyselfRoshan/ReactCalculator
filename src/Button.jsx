a2fb3762e144084d5396475ed20523710b980a9c
MyselfRoshan <roshanghimire699@gmail.com>
2023-03-23 18:19:40 +0545

    feat: added functionalities to AC,C,+/- buttons

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,11 +1,22 @@
 function Button({ butttonTxt, result, setResult }) {
   function handleClick(e) {
+    const targetBtnTxt = e.target.innerText;
     setResult((prevResult) => {
-      const regexNumber = /d[0-9]|./;
-      if (regexNumber.test(e.target.innerText))
-        prevResult.concat(e.target.innerText);
+      // const regexNumber = /[0-9]|./;
+      if (targetBtnTxt === "AC") return "";
+      if (targetBtnTxt === "+/-") return `${Number(prevResult) * -1}`;
+      if (targetBtnTxt === "C" && !prevResult.length)
+        return prevResult.slice(0, prevResult.length - 1);
+      // if (regexNumber.test(targetBtnTxt)) {
+      // console.log(targetBtnTxt);
+
+      return prevResult.concat(e.target.innerText);
+      // return !prevResult ? targetBtnTxt : prevResult.concat(targetBtnTxt);
     });
-    console.log(result);
+    // let operators = result.split(/(?<!\\d)-?[0-9.]+/);
+    // let operators = result.split(/(?<=\\d)[-+/%*]/);
+    let operators = result.split(/[–+÷%×]/);
+    console.log(operators);
   }
   return (
     <button className="button" onClick={(e) => handleClick(e)}>
