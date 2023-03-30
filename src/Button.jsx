4062e9f994e25906d4506fc15e7a8ed688013bd7
MyselfRoshan <roshanghimire699@gmail.com>
2023-03-30 22:43:56 +0545

    refactor: css and font

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,5 +1,9 @@
 function Button({ butttonTxt, setResult, inputRef, result }) {
   function handleClick(e) {
+    //! Run inputRef only one time for focus
+    // if(inputRef)
+    console.log(inputRef.current.matches(":focus"));
+    // if(!inputRef.current);
     inputRef.current.focus();
     const targetBtnTxt = e.target.innerText;
     setResult((prevResult) => {
@@ -9,12 +13,8 @@ function Button({ butttonTxt, setResult, inputRef, result }) {
       }
       if (targetBtnTxt === "C" && prevResult.length >= 0)
         return prevResult.slice(0, prevResult.length - 1);
-      // return prevResult !== null
-      return prevResult.concat(e.target.innerText);
-      // : result;
-      // return prevResult !== null
-      //   ? prevResult.concat(e.target.innerText)
-      //   : result;
+      if (result === "Math Error!") return targetBtnTxt;
+      return prevResult.concat(targetBtnTxt);
     });
   }
   return (
